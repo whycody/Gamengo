680955148b9bd807bfeb815b9bce83d065a21b35
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2024-05-13 14:15:44 +0200

    Adding default speed and jumpspeed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,45 +2,45 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpspeed;
-    private Rigidbody2D body;
-    private Animator anim;
-    private bool grounded;
+    [SerializeField] private float speed = 8;
+    [SerializeField] private float jumpSpeed = 8;
+    private Rigidbody2D _body;
+    private Animator _anim;
+    private bool _grounded;
 
     private void Awake()
     {
-        body = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        _body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, _body.velocity.y);
 
         if (horizontalInput > 0.01f)
             transform.localScale = new Vector2(10, 10);
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector2(-10, 10);
 
-        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && _grounded)
             Jump();
 
-        anim.SetBool("running", horizontalInput != 0);
+        _anim.SetBool("running", horizontalInput != 0);
     }
 
     private void Jump()
     {
-        anim.SetBool("jumping", true);
-        body.velocity = new Vector2(body.velocity.x, jumpspeed);
-        grounded = false;
+        _anim.SetBool("jumping", true);
+        _body.velocity = new Vector2(_body.velocity.x, jumpSpeed);
+        _grounded = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        anim.SetBool("jumping", false);
-        grounded = true;
+        _anim.SetBool("jumping", false);
+        _grounded = true;
     }
 }
