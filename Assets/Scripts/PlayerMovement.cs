b690c6d4ca7091f5450aac669a6dc573b558b483
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2024-05-14 20:16:13 +0200

    Added "Ground" tag to grounds, fixed jumping, sped up getBool() method usage, replaced SetBool() to CompareTag() for performance, moved walk animation to Animations folder

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,14 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D _body;
     private Animator _anim;
     private bool _grounded;
+    
+    // many people say that's a lot faster to give int to getBool() instead of string
+    private static readonly int RunningParam = Animator.StringToHash("running");
+    private static readonly int WalkingParam = Animator.StringToHash("walking");
+    private static readonly int JumpingParam = Animator.StringToHash("jumping");
+    private static readonly int JumpParam = Animator.StringToHash("jump");
+    private static readonly int GroundedParam = Animator.StringToHash("grounded");
+
 
     private void Awake()
     {
@@ -17,8 +25,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+        //float verticalInput = Input.GetAxis("Vertical");
         _body.velocity = new Vector2(horizontalInput * speed, _body.velocity.y);
 
         // turning right and left
@@ -26,29 +34,33 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector2(10, 10);
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector2(-10, 10);
-
+        
+        // Jumping handling
         if (Input.GetKeyDown(KeyCode.UpArrow) && _grounded)
             Jump();
         
+        // Setting gravity force on Down Arrow
         _body.gravityScale = Input.GetKey(KeyCode.DownArrow) ? 4f : 1.5f;
 
-        speed = _anim.GetBool("running") ? 8 : 6;
+        speed = _anim.GetBool(RunningParam) ? 8 : 6;
 
-        _anim.SetBool("running", Input.GetKey(KeyCode.LeftShift) && horizontalInput != 0);
-
-        _anim.SetBool("walking", horizontalInput != 0);
+        // Updating Animator parameters
+        _anim.SetBool(RunningParam, Input.GetKey(KeyCode.LeftShift) && horizontalInput != 0);
+        _anim.SetBool(WalkingParam, horizontalInput != 0);
     }
 
     private void Jump()
     {
-        _anim.SetBool("jumping", true);
+        _anim.SetBool(JumpingParam, true);
+        _anim.SetTrigger(JumpParam);
         _body.velocity = new Vector2(_body.velocity.x, jumpSpeed);
         _grounded = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        _anim.SetBool("jumping", false);
+        if (!collision.gameObject.CompareTag("Ground")) return;
+        _anim.SetBool(JumpingParam, false);
         _grounded = true;
     }
 }
