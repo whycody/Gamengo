58bbdd10ed3f2aed0aae0dcda74467bd8099da52
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2024-05-15 21:37:40 +0200

    Added laying anim when idle and DownArrow, added regions for code clarity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,18 +3,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    #region Variables
+
     [SerializeField] private float speed = 8;
     [SerializeField] private float jumpSpeed = 8;
+    [SerializeField] private float idleAnimChangeTime = 3;
     private Rigidbody2D _body;
     private Animator _anim;
     private bool _grounded;
-    
+
+    #endregion
+
+    #region Parameters
+
     // many people say that's a lot faster to give int to getBool() instead of string
     private static readonly int RunningParam = Animator.StringToHash("running");
     private static readonly int WalkingParam = Animator.StringToHash("walking");
     private static readonly int JumpingParam = Animator.StringToHash("jumping");
     private static readonly int JumpParam = Animator.StringToHash("jump");
-    private static readonly int GroundedParam = Animator.StringToHash("grounded");
+    private static readonly int IdleParam = Animator.StringToHash("idle");
+    private static readonly int LayingParam = Animator.StringToHash("laying");
+    //private static readonly int GroundedParam = Animator.StringToHash("grounded");
+
+    #endregion
 
 
     private void Awake()
@@ -25,8 +36,9 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        #region Movement handling
+
         var horizontalInput = Input.GetAxis("Horizontal");
-        //float verticalInput = Input.GetAxis("Vertical");
         _body.velocity = new Vector2(horizontalInput * speed, _body.velocity.y);
 
         // turning right and left
@@ -34,19 +46,31 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector2(10, 10);
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector2(-10, 10);
-        
+
         // Jumping handling
         if (Input.GetKeyDown(KeyCode.UpArrow) && _grounded)
             Jump();
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && _anim.GetBool(IdleParam))
+            _anim.SetBool(LayingParam, true);
         
         // Setting gravity force on Down Arrow
         _body.gravityScale = Input.GetKey(KeyCode.DownArrow) ? 4f : 1.5f;
 
         speed = _anim.GetBool(RunningParam) ? 8 : 6;
 
+        #endregion
+
+        #region Updating parameters
+
         // Updating Animator parameters
         _anim.SetBool(RunningParam, Input.GetKey(KeyCode.LeftShift) && horizontalInput != 0);
         _anim.SetBool(WalkingParam, horizontalInput != 0);
+        _anim.SetBool(IdleParam,
+            !(_anim.GetBool(RunningParam) || _anim.GetBool(WalkingParam) || _anim.GetBool(JumpingParam)));
+        if(!_anim.GetBool(IdleParam)) _anim.SetBool(LayingParam, false);
+        
+        #endregion
     }
 
     private void Jump()
