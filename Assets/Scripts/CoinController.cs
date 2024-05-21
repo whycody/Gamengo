7c7bb50c917a004b9606fb2ce8da69934ee01d85
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2024-05-22 00:00:09 +0200

    temp - changes to commit:
    - Added "Player" tag to Player
    - Added CoinController script to Coin prefab
    - Added CoinsManager script to GameManager object
    - Added TextMesh Pro asset
    - Added Coins Display to Canva
    
    Issue - 2 times too much coins on ChangeCoins method

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -1,14 +1,23 @@
+using System;
 using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
+    [SerializeField] private int value = 1;
+    private bool _hasTriggered;
+
+    private CoinsManager _coinManager;
+
     private void Start()
     {
-        
+        _coinManager = CoinsManager.Instance;
     }
 
-    private void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (!other.CompareTag("Player") || _hasTriggered) return;
+        _hasTriggered = true;
+        _coinManager.ChangeCoins(value);
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/CoinsManager.cs
@@ -0,0 +1,29 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class CoinsManager : MonoBehaviour
+{
+    public static CoinsManager Instance;
+
+    private int _coins;
+    [SerializeField] private TMP_Text coinsDisplay;
+
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+    }
+
+    public void ChangeCoins(int amount)
+    {
+        _coins += amount;
+    }
+
+    private void OnGUI()
+    {
+        coinsDisplay.text = _coins.ToString();
+    }
+}
